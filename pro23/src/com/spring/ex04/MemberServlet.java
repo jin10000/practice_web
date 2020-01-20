032c683f9120413f274a14a9477b876dc1210f2b
jin10000 <56944002+jin10000@users.noreply.github.com>
2020-01-20 13:53:54 +0900

    Add files via upload

---
+++ pro23/src/com/spring/ex04/MemberServlet.java
@@ -0,0 +1,154 @@
+package com.spring.ex04;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.spring.ex01.MemberVO;
+
+@WebServlet("/mem4.do")
+public class MemberServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doHandle(request, response);
+	}
+
+	private void doHandle(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		MemberDAO dao = new MemberDAO();
+		MemberVO memberVO = new MemberVO();
+		String action = request.getParameter("action");
+		String nextPage = "";
+
+		if (action == null || action.equals("listMembers")) {
+
+			List<MemberVO> membersList = dao.selectAllMemberList();
+			request.setAttribute("membersList", membersList);
+			nextPage = "test03/listMembers.jsp";
+
+		} else if (action.equals("selectMemberById")) {
+
+			String id = request.getParameter("value");
+			memberVO = dao.selectMemberById(id);
+			request.setAttribute("member", memberVO);
+			nextPage = "test03/memberInfo.jsp";
+
+		} else if (action.equals("selectMemberByPwd")) {
+
+			int pwd = Integer.parseInt(request.getParameter("value"));
+			List<MemberVO> membersList = dao.selectMemberByPwd(pwd);
+			request.setAttribute("membersList", membersList);
+			nextPage = "test03/listMembers.jsp";
+
+		} else if (action.equals("insertMember")) {
+
+			String id = request.getParameter("id");
+			String pwd = request.getParameter("pwd");
+			String name = request.getParameter("name");
+			String email = request.getParameter("email");
+			memberVO.setId(id);
+			memberVO.setPwd(pwd);
+			memberVO.setName(name);
+			memberVO.setEmail(email);
+			dao.insertMember(memberVO);
+			nextPage = "/mem4.do?action=listMembers";
+			
+		} else if(action.equals("updateMember")){
+			
+	          String id=request.getParameter("id");
+	          String pwd=request.getParameter("pwd");
+	          String name=request.getParameter("name");
+	          String email = request.getParameter("email");
+	          memberVO.setId(id);
+	          memberVO.setPwd(pwd);
+	          memberVO.setName(name);
+	          memberVO.setEmail(email);
+	          dao.updateMember(memberVO);
+	          nextPage="/mem4.do?action=listMembers";  
+	          
+	      } else if(action.equals("deleteMember")) {
+	    	  
+	    	  String id = request.getParameter("id");
+	    	  dao.deleteMember(id);
+	    	  nextPage = "/mem4.do?action=listMembers";
+	    	  
+	      } else if(action.equals("searchMember")) {
+	    	  
+	    	  String name = request.getParameter("name");
+	    	  String email = request.getParameter("email");
+	    	  memberVO.setName(name);
+	    	  memberVO.setEmail(email);
+	    	  List<MemberVO> membersList = dao.searchMember(memberVO);
+	    	  request.setAttribute("membersList", membersList);
+	    	  nextPage = "test03/listMembers.jsp";
+	    	  
+	      } else if(action.equals("foreachSelect")) {
+	    	  
+	    	  List<String> nameList = new ArrayList<String>();
+	    	  nameList.add("±è¾¾");
+	    	  nameList.add("ÀÌ¾¾");
+	    	  nameList.add("¹Ú¾¾");
+	    	  List<MemberVO> membersList = dao.foreachSelect(nameList);
+	    	  request.setAttribute("membersList", membersList);
+	    	  nextPage = "test03/listMembers.jsp";
+	    	  
+	      } else if(action.equals("foreachInsert")) {
+	    	  
+	    	  List<MemberVO> memList = new ArrayList<MemberVO>();
+	    	  memList.add(new MemberVO("m1", "2344", "¹Ú±æµ¿", "m1@test.com"));
+	    	  memList.add(new MemberVO("m2", "3455", "ÀÌ±æµ¿", "m2@test.com"));
+	    	  memList.add(new MemberVO("m3", "4566", "±è±æµ¿", "m3@test.com"));
+	    	  int result = dao.foreachInsert(memList);
+	    	  nextPage = "/mem4.do?action=listMembers";
+	    	  
+	      }
+
+		RequestDispatcher dispatch = request.getRequestDispatcher(nextPage);
+		dispatch.forward(request, response);
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
